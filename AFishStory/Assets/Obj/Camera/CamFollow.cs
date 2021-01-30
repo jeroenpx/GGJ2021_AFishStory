c6d389db383b1f82634d66e954fa412ff628e8be
jeroenpx <jeroenpx@users.noreply.github.com>
2021-01-30 01:07:41 +0100

    [A Fish Story] Swimming (+ dash)

---
+++ AFishStory/Assets/Obj/Camera/CamFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = .4f;
+
+    private float zDepth;
+    private Vector3 currentVelocity;
+    public float lookaheadAmount = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zDepth = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Adapt based on mouse position vs center
+        Vector3 cursorpos = Input.mousePosition / new Vector2(Screen.height, Screen.height) * 2f - Vector2.one;
+        float dist = cursorpos.magnitude;
+        if(dist>1) {
+            cursorpos /= dist;
+        }
+
+        // Fish target position
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, zDepth);
+
+        // Combined Target (fish + look around)
+        Vector3 combinedTarget = targetPos + cursorpos * lookaheadAmount;
+
+        transform.position = Vector3.SmoothDamp(transform.position, combinedTarget, ref currentVelocity, smoothTime);
+    }
+}
---
+++ AFishStory/Assets/Obj/Fish/PlayerControls.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private float startHoldDownTime;
+
+    public float dashTreshhold = .3f;
+    public float dashDuration = .3f;
+    public float dashForce = 100f;
+    public float swimForce = 100f;
+
+    public Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Vector3 GetMouseInScene() {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0;
+        return worldPosition;
+    }
+
+    IEnumerator DashCoroutine () {
+        Debug.Log("[Dash] Start");
+        float startTime = Time.time;
+        Vector3 initialDir = GetMouseInScene() - transform.position;
+
+
+        while(Time.time - startTime < dashDuration) {
+            yield return new WaitForFixedUpdate();
+            Vector3 dir = GetMouseInScene() - transform.position;
+            body.AddForce(dir.normalized*dashForce);
+        }
+
+        Debug.Log("[Dash] End");
+    }
+
+    void DoDash() {
+        // Execute Dash!
+        StartCoroutine(DashCoroutine ());
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            // Click
+            startHoldDownTime = Time.time;
+        }
+
+        if(Input.GetMouseButtonUp(0)) {
+            // Release button -> potentially "swooosh!"
+            float clickDuration = Time.time - startHoldDownTime;
+            if(clickDuration < dashTreshhold) {
+                // Dash
+                DoDash();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0)) {
+            Vector3 dir = GetMouseInScene() - transform.position;
+            body.AddForce(dir.normalized*swimForce);
+        }
+    }
+}
